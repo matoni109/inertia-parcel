554b265adaeed821cfc001a9171fd3be9dddfeb3
Chris Wade <chris.wade@discolabs.com>
2021-12-04 11:38:54 +1100

    check tailwind form plugin

--- app/Pages/InertiaExample.js
+++ app/Pages/InertiaExample.js
@@ -30,6 +30,27 @@ const InertiaExample = () => {
           <div className="ml-3">
             <h3 className="font-semibold"> Lana Del Rey T12</h3>
             <p className="text-gray-500"> Singer/songwriter </p>
+            <label className="block">
+              <span className="text-gray-700">Input (datetime-local)</span>
+              <input
+                type="datetime-local"
+                className="form-input mt-1 block w-full"
+              />
+            </label>
+            <article class="prose lg:prose-xl">
+              <h1>Garlic bread with cheese: What the science tells us</h1>
+              <p>
+                For years parents have espoused the health benefits of eating
+                garlic bread with cheese to their children, with the food
+                earning such an iconic status in our culture that kids will
+                often dress up as warm, cheesy loaf for Halloween.
+              </p>
+              <p>
+                But a recent study shows that the celebrated appetizer may be
+                linked to a series of rabies cases springing up around the
+                country.
+              </p>
+            </article>
           </div>
         </div>
         <Card sectioned>
